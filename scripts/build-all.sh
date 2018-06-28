49f653f4f54b49d5f323d190d629b31f0e822272
simoncurran <s.curran@liberty-it.co.uk>
2018-06-28 14:30:30 +0100

    Added.

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -1,5 +1,7 @@
-cd ../product-header; npm run build-prod; cd ../productmgt
+cd ../product-header; npm run build-prod; cd ../productmgt;
 
-cd ../product-cart; npm run build-prod; cd ../productmgt
+cd ../product-cart; npm run build-prod; cd ../productmgt;
 
-cd ../product-view2; npm run build-prod; cd ../productmgt
+cd ../product-view2; npm run build-prod; cd ../productmgt;
+
+npm run build-prod;
---
+++ scripts/init-all.sh
@@ -0,0 +1,8 @@
+cd ../product-header; yarn install; yarn install --save-dev; yarn run build-prod; cd ../productmgt;
+
+cd ../product-cart; yarn install; yarn install --save-dev; yarn run build-prod; cd ../productmgt;
+
+cd ../product-view2; yarn install; yarn install --save-dev; yarn run build-prod; cd ../productmgt;
+
+yarn install; yarn install --save-dev; yarn run build-prod;
+
