a85fbdc2231306d1339884689373ccfa72f607aa
simoncurran <s.curran@liberty-it.co.uk>
2018-06-28 15:59:50 +0100

    Updates.

--- src/app/webcomponents/script.store.ts
+++ src/app/webcomponents/script.store.ts
@@ -4,6 +4,6 @@ interface Scripts {
 }  
 export const ScriptStore: Scripts[] = [
     {name: 'product-header', src: 'http://localhost:8081/product-header.js'},
-    {name: 'product-view', src: 'http://localhost:8082/product-view.js'},
+    {name: 'product-view', src: 'http://localhost:8082/product-view2.js'},
     {name: 'product-cart', src: 'http://localhost:8083/product-cart.js'}
 ];
